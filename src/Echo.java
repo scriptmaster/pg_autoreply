16ea514f53e7b8bfbb573a6a7a90587534dd688f
ScriptMaster <scriptmaster@gmail.com>
2013-06-26 04:23:10 +0530

    Update Echo.java

--- src/Echo.java
+++ src/Echo.java
@@ -22,7 +22,7 @@ public class Echo extends CordovaPlugin {
 
     private void echo(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) { 
-            callbackContext.success(message);
+            callbackContext.success(message.replace('e','i'));
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
